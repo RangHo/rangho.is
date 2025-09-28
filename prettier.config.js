f2945bec758e4518c793a3d8049cd4d924146c04
RangHo Lee <hello@rangho.me>
2025-09-28 17:53:44 +0900

    chore: change formatting and linting settings

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,7 @@
 /** @type {import('prettier').Config} */
 const config = {
-  semi: true,
-  useTabs: false,
-  singleQuote: true,
-  trailingComma: 'es5',
-  printWidth: 100,
-  plugins: ['prettier-plugin-svelte'],
-  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+  plugins: ["prettier-plugin-svelte"],
+  overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
 };
 
 export default config;
