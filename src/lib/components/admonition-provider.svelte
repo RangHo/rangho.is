05ba74b976c187b7b771f551bbac631663fc8647
RangHo Lee <hello@rangho.me>
2025-10-22 11:40:18 +0900

    feat: separate admonition styles to its own component

---
+++ src/lib/components/admonition-provider.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  interface Props {
+    children: Snippet
+  }
+
+  let { children }: Props = $props();
+</script>
+
+{@render children()}
+
+<style>
+  :global {
+    /** Note */
+    .note {
+      @apply border-l-4 border-blue-500 bg-blue-50 dark:bg-blue-900/30 p-4 my-4;
+    }
+    .note p:first-of-type {
+      @apply mt-0;
+    }
+    .note p:last-of-type {
+      @apply mb-0;
+    }
+
+    /** Tip */
+    .tip {
+      @apply border-l-4 border-green-500 bg-green-50 dark:bg-green-900/30 p-4 my-4;
+    }
+    .tip p:first-of-type {
+      @apply mt-0;
+    }
+    .tip p:last-of-type {
+      @apply mb-0;
+    }
+
+    /** Important */
+    .important {
+      @apply border-l-4 border-purple-500 bg-purple-50 dark:bg-purple-900/30 p-4 my-4;
+    }
+    .important p:first-of-type {
+      @apply mt-0;
+    }
+    .important p:last-of-type {
+      @apply mb-0;
+    }
+
+    /** Warning */
+    .warning {
+      @apply border-l-4 border-yellow-500 bg-yellow-50 dark:bg-yellow-900/30 p-4 my-4;
+    }
+    .warning p:first-of-type {
+      @apply mt-0;
+    }
+    .warning p:last-of-type {
+      @apply mb-0;
+    }
+
+    /** Caution */
+    .caution {
+      @apply border-l-4 border-red-500 bg-red-50 dark:bg-red-900/30 p-4 my-4;
+    }
+    .caution p:first-of-type {
+      @apply mt-0;
+    }
+  }
+</style>
--- src/routes/thinking/[slug]/+page.svelte
+++ src/routes/thinking/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { PageProps } from "./$types";
 
+  import AdmonitionProvider from "$lib/components/admonition-provider.svelte";
   import Comment from "$lib/components/comment.svelte";
 
   let { data }: PageProps = $props();
@@ -8,14 +9,15 @@
 </script>
 
 <main
-  class="mx-auto px-4 prose prose-teal prose-sm md:prose-base lg:prose-lg xl:prose-xl dark:prose-invert"
+  class="mx-auto my-5 px-4
+  prose prose-teal prose-sm md:prose-base lg:prose-lg xl:prose-xl dark:prose-invert"
 >
+  <hgroup class="pb-5 mb-10 font-head">
+    <h1 class="my-0">{metadata.title}</h1>
+    <p class="my-5">{metadata.subtitle}</p>
+  </hgroup>
   <article class="my-20">
-    <hgroup class="pb-5 mb-10 font-head text-center">
-      <h1 class="my-0">{metadata.title}</h1>
-      <p class="my-5">{metadata.subtitle}</p>
-    </hgroup>
-    <Thought />
+    <AdmonitionProvider><Thought /></AdmonitionProvider>
   </article>
   <Comment
     repo="RangHo/thoughts"
@@ -33,59 +35,5 @@
 <style>
   hgroup {
     border-bottom: 5px double var(--un-prose-hr);
-  }
-
-  :global {
-    /** Note */
-    .note {
-      @apply border-l-4 border-blue-500 bg-blue-50 dark:bg-blue-900/30 p-4 my-4;
-    }
-    .note p:first-of-type {
-      @apply mt-0;
-    }
-    .note p:last-of-type {
-      @apply mb-0;
-    }
-
-    /** Tip */
-    .tip {
-      @apply border-l-4 border-green-500 bg-green-50 dark:bg-green-900/30 p-4 my-4;
-    }
-    .tip p:first-of-type {
-      @apply mt-0;
-    }
-    .tip p:last-of-type {
-      @apply mb-0;
-    }
-
-    /** Important */
-    .important {
-      @apply border-l-4 border-purple-500 bg-purple-50 dark:bg-purple-900/30 p-4 my-4;
-    }
-    .important p:first-of-type {
-      @apply mt-0;
-    }
-    .important p:last-of-type {
-      @apply mb-0;
-    }
-
-    /** Warning */
-    .warning {
-      @apply border-l-4 border-yellow-500 bg-yellow-50 dark:bg-yellow-900/30 p-4 my-4;
-    }
-    .warning p:first-of-type {
-      @apply mt-0;
-    }
-    .warning p:last-of-type {
-      @apply mb-0;
-    }
-
-    /** Caution */
-    .caution {
-      @apply border-l-4 border-red-500 bg-red-50 dark:bg-red-900/30 p-4 my-4;
-    }
-    .caution p:first-of-type {
-      @apply mt-0;
-    }
   }
 </style>
