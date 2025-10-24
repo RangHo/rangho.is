9486e46b166eafcc990c12f972af0c01aaed8d46
RangHo Lee <hello@rangho.me>
2025-10-24 17:24:20 +0900

    style: fix format

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,6 +1,9 @@
-<footer class="w-full bg-gray-300/20 py-5 text-center text-gray-600 md:py-10 lg:py-15 xl:py-20 dark:(bg-gray-700/20 text-gray-400)">
-  <p class="text-lg">
-    Built by RangHo with <span class="i i-simple-icons:svelte"></span> and <span class="i i-tabler:heart"></span>
+<footer
+  class="w-full bg-gray-300/20 py-5 text-center text-sm text-gray-600 md:(py-10 text-base) lg:(py-15 text-lg) xl:(py-20 text-xl) dark:(bg-gray-700/20 text-gray-400)"
+>
+  <p>
+    Built by RangHo with <span class="i i-simple-icons:svelte"></span> and
+    <span class="i i-tabler:heart"></span>
   </p>
 </footer>
 
--- src/routes/thinking/+page.svelte
+++ src/routes/thinking/+page.svelte
@@ -12,14 +12,23 @@
       class="text-inherit no-underline"
       href={resolve("/thinking/[slug]", { slug: entry.slugified })}
     >
-      <div class="m-10 border rounded-md p-4 text-sm md:(text-base p-6) lg:(text-lg p-8) xl:(text-xl p-10)">
-        <h1 class="font-head text-xl md:(mb-3 text-2xl) lg:(mb-4 text-3xl) xl:(mb-5 text-4xl)">
+      <div
+        class="m-10 border rounded-md p-4 text-sm md:(text-base p-6) lg:(text-lg p-8) xl:(text-xl p-10)"
+      >
+        <h1
+          class="font-head text-xl md:(mb-3 text-2xl) lg:(mb-4 text-3xl) xl:(mb-5 text-4xl)"
+        >
           {entry.title}
         </h1>
         <hr class="my-3 text-gray-500/50" />
-        <ul class="metadata my-3 flex flex-row text-gray-600 dark:text-gray-400">
+        <ul
+          class="metadata my-3 flex flex-row text-gray-600 dark:text-gray-400"
+        >
           {#if entry.date}
-            <li><span class="i i-tabler:calendar"></span> {new Date(entry.date).toDateString()}</li>
+            <li>
+              <span class="i i-tabler:calendar"></span>
+              {new Date(entry.date).toDateString()}
+            </li>
           {/if}
           {#if entry.category}
             <li><span class="i i-tabler:category"></span> {entry.category}</li>
