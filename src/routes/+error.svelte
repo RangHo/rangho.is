818d6544d6907ecee9ca8ec02239f8a884a3fc6e
RangHo Lee <hello@rangho.me>
2026-02-14 21:09:33 +0900

    feat: generate an error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Header from "$lib/components/header.svelte";
+</script>
+
+<Header something="not-found" />
+
+<main class="flex h-screen w-full items-center justify-center">
+  <h1 class="text-4xl font-bold">404 Not Found</h1>
+</main>
