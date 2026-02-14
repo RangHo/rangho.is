dd2bf04d9e391e7509e29a1493312065395058fd
RangHo Lee <hello@rangho.me>
2026-02-14 21:09:16 +0900

    fix: allow specifying arbitrary string

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-  import { resolve } from "$app/paths";
+  import { somethings } from "$lib/data";
+
   import ColorSchemeToggle from "$lib/components/color-scheme-toggle.svelte";
 
   interface Props {
-    something: "knowing" | "talking" | "thinking";
+    something: string;
   }
 
   let { something }: Props = $props();
+
+  let href = $derived(somethings.includes(something) ? `/${something}` : "/");
 </script>
 
 <header class="sticky top-0 z-9999 bg-transparent backdrop-blur-xl">
@@ -18,11 +21,11 @@
     <div
       class="font-head font-bold flex items-center space-x-3 text-2xl md:text-3xl lg:text-4xl xl:text-5xl"
     >
-      <a class="mr-2 text-gray-500/50" href={resolve("/")}>rangho.is</a>
+      <a class="mr-2 text-gray-500/50" href="/">rangho.is</a>
       <span class="mr-2 text-gray-800/50 dark:text-gray-200/50">/</span>
       <a
         class="font-extrabold text-gray-800 dark:text-gray-200"
-        href={resolve(`/${something}`)}
+        {href}
       >
         {something}
       </a>
