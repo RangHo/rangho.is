813281dec129b1b2c51626c1f0b5879ed4a11b9b
Juhun Lee <rangho@yonsei.ac.kr>
2026-05-14 16:53:01 +0900

    build: initialize Wrangler for Cloudflare Workers deployment

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,5 @@
 import type { Component } from "svelte";
 import type { OrgMetadata } from "ox-svelte";
-
 import pkg from "../package.json";
 
 declare global {
@@ -9,17 +8,32 @@ declare global {
   //   interface Locals {}
   //   interface PageData {}
   //   interface PageState {}
-  //   interface Platform {}
   // }
-
   declare const __DEPENDENCIES__: typeof pkg.dependencies;
 
   declare const __DEV_DEPENDENCIES__: typeof pkg.devDependencies;
 
-  declare module "*.org" {
+  declare "*.org" {
     const component: Component;
+
     export default component;
 
     export const metadata: OrgMetadata;
   }
+
+  namespace App {
+    interface Platform {
+      env: Env;
+      ctx: ExecutionContext;
+      caches: CacheStorage;
+      cf?: IncomingRequestCfProperties
+    }
+  }
+
+  // namespace App {
+  //   interface Error {}
+  //   interface Locals {}
+  //   interface PageData {}
+  //   interface PageState {}
+  // }
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
 import { orgPreprocess } from "svelte-preprocess-org";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,43 +13,29 @@ const config = {
         "$lib/components/code.svelte": "Code",
         "$lib/components/math.svelte": "Math",
         "$lib/components/org-image.svelte": "OrgImage",
-        "$lib/components/org-link.svelte": "OrgLink",
+        "$lib/components/org-link.svelte": "OrgLink"
       },
       idLocations: ["src/data/knowledges/*.org", "src/data/thoughts/*.org"],
       imageFormat: '<OrgImage src=\\"%s\\" alt=\\"%s\\" />',
       initDirectory: ".emacs.d/",
       latexEnvironmentFormat: "<Math expression={%s} displayMode />",
-      latexDisplayFragmentFormat:
-        "<Math expression={%s} displayMode inlineMode />",
+      latexDisplayFragmentFormat: "<Math expression={%s} displayMode inlineMode />",
       latexInlineFragmentFormat: "<Math expression={%s} inlineMode />",
-      metadataExportList: [
-        "title",
-        "subtitle",
-        "description",
-        "date",
-        "language",
-      ],
+      metadataExportList: ["title", "subtitle", "description", "date", "language"],
       srcBlockFormat: '<Code lang=\\"%s\\" code={%s} />',
       withSectionNumbers: false,
-      withBrokenLinks: true,
+      withBrokenLinks: true
     }),
-    vitePreprocess(),
+    vitePreprocess()
   ],
   kit: {
-    adapter: adapter({
-      fallback: "404.html",
-    }),
-    alias: {
-      $data: "src/data",
-    },
+    adapter: adapter(),
+    alias: { $data: "src/data" },
     paths: {
-      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
     },
-    prerender: {
-      handleMissingId: "warn",
-      handleUnseenRoutes: "warn",
-    },
-  },
+    prerender: { handleMissingId: "warn", handleUnseenRoutes: "warn" }
+  }
 };
 
 export default config;
